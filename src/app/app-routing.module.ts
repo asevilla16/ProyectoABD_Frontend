0c2158d75c6ce775eb2fc0b87ae61b70825d23d7
Maria Salinas <mariasalinas713@gmail.com>
2020-09-23 16:25:59 -0600

    -Agregado el componente login con validaciones de acceso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { AdminMastersComponent } from './admin-masters/admin-masters.component';
+import { EmployeeGuard } from './guards/empleado.guard';
+import { SuperAdminGuard } from './guards/superadmin.guard';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SuperAdminDashboardComponent } from './super-admin-dashboard/super-admin-dashboard.component';
+import { UserComponent } from './user/user.component';
+const routes: Routes = [
+  { path: 'employee', component: UserComponent },
+  { path: 'home', component: HomepageComponent },
+  {
+    path: 'superAdmin',
+    component: SuperAdminDashboardComponent,
+    canActivate: [SuperAdminGuard],
+  },
+  {
+    path: 'admin',
+    component: AdminMastersComponent,
+    canActivate: [EmployeeGuard],
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SuperAdminDashboardComponent } from './super-admin-dashboard/super-admin-dashboard.component';
+import { AdminMastersComponent } from './admin-masters/admin-masters.component';
+import { LoginService } from './services/login.service';
+import { UserService } from './services/user.service';
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    UserComponent,
+    HomepageComponent,
+    SuperAdminDashboardComponent,
+    AdminMastersComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule, 
   ],
-  providers: [],
+  exports: [
+    UserComponent,
+  ],
+  providers: [
+    UserService,
+    LoginService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
